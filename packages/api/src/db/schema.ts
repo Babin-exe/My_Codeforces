dc85775110c392b8a6e5cf8e8d518076cae29093
Babin-exe <nepalbabin4@gmail.com>
2026-09-22 23:03:20 +0545

    schema design for problem_versions table , only basic ideas

--- packages/api/src/db/schema.ts
+++ packages/api/src/db/schema.ts
@@ -2,6 +2,10 @@ export const USER_ROLE = `
 CREATE TYPE user_role as ENUM('USER' , 'ADMIN');
 `;
 
+export const CHECKER_TYPE = `CREATE TYPE checker_type as ENUM ('STANDARD' , 'INTERACTIVE' , 'SPECIAL' )`;
+
+export const STATUS = `CREATE TYPE status AS ENUM('DRAFT' , 'IN_REVIEW' , 'PUBLISHED' , 'ARCHIVED')`;
+
 export const USER = `
 CREATE TABLE users(
 id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -34,6 +38,122 @@ avatar_url VARCHAR(255),
 role user_role NOT NULL DEFAULT 'USER',
 
 updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+
+);
+`;
+
+
+export const PROBLEMS = `
+CREATE TABLE problems(
+
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+    slug TEXT UNIQUE NOT NULL,
+
+    created_by UUID NOT NULL,
+
+    CONSTRAINT fk_problems_created_by
+        FOREIGN KEY (created_by)
+        REFERENCES users(id)
+        ON DELETE RESTRICT,
+
+    visibility BOOLEAN NOT NULL DEFAULT FALSE,
+
+    status status NOT NULL DEFAULT 'DRAFT',
+
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+    deleted_at TIMESTAMPTZ
+
+);
+`;
+
+
+/*
+ fk_table_name_column name
+ */
+
+
+export const PROBLEM_VERSIONS = `
+
+CREATE TABLE problem_versions (
+
+id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+problem_id UUID NOT NULL,
+
+CONSTRAINT  fk_problem_version_problem_id
+FOREIGN KEY (problem_id)
+REFERENCES problems(id)
+ ON DELETE RESTRICT ,
+
+version_number INTEGER NOT NULL ,
+
+created_by UUID NOT NULL ,
+
+CONSTRAINT fk_problem_version_created_by 
+FOREIGN KEY (created_by) 
+REFERENCES users(id),
+
+created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+title TEXT NOT NULL ,
+
+CONSTRAINT uq_problem_version
+ UNIQUE(problem_id , version_number),
+
+time_limit_ms INTEGER NOT NULL DEFAULT 1000,
+
+memory_limit_mb INTEGER NOT NULL DEFAULT 256,
+
+statement TEXT NOT NULL ,
+
+output_limit_mb INTEGER NOT NULL DEFAULT 1,
+
+
+difficulty INTEGER NOT NULL DEFAULT 800,
+
+checker_type checker_type NOT NULL DEFAULT 'STANDARD' ,
+
+testcase_count INTEGER NOT NULL ,
+
+testcase_s3_key VARCHAR(256) NOT NULL ,
+
+testcase_version INTEGER NOT NULL ,
+
+
+is_active BOOLEAN NOT NULL ,
+
+
+CHECK (
+difficulty >=800 AND difficulty <= 3500
+) , 
+
+CHECK (
+time_limit_ms >=1000 AND time_limit_ms <=3000
+),
+
+CHECK (
+memory_limit_mb>=256
+AND memory_limit_mb <= 512
+AND output_limit_mb >= 1
+AND output_limit_mb <= 64
+),
+
+CHECK (
+version_number > 0 
+) ,
+
+CHECK (
+testcase_count > 0 
+), 
+
+
+CHECK (
+testcase_version > 0
+)
 
 );
 `;
